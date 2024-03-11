cc1a05b3b274be2c04f87801224238803ba27777
Crowdbotics <team@crowdbotics.com>
2024-03-11 01:48:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-haze-46915",
+  url: "https://tiny-haze-46915.botics.co",
+  api: "https://tiny-haze-46915.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
